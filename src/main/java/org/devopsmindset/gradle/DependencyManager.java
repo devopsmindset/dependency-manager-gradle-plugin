acbce601bb24f001c72a3ba19cf0861aa31edf5c
Anyul Rivas <anyul.rivas_oropeza@roche.com>
2020-11-19 12:35:34 +0100

    reducing cyclomatic complexity

--- src/main/java/org/devopsmindset/gradle/DependencyManager.java
+++ src/main/java/org/devopsmindset/gradle/DependencyManager.java
@@ -3,6 +3,7 @@ package org.devopsmindset.gradle;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Preconditions;
 import org.apache.commons.io.FileUtils;
 import org.devopsmindset.gradle.compress.CompressionUtils;
 import org.devopsmindset.gradle.model.DependencyManagerExtension;
@@ -20,12 +21,10 @@ import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Objects.requireNonNull;
-
 public class DependencyManager extends DefaultTask {
 
-    final String DEFAULT_LOCATION = "dependency-manager";
     public static final String DEFAULT_DEPENDENCY_FILE = "processed.dependencies";
+    final String DEFAULT_LOCATION = "dependency-manager";
     final String DEFAULT_DEPENDENCY_BASE_FILE = "base.dependencies";
     final String BASE_CONFIGURATION = "base";
 
@@ -39,47 +38,39 @@ public class DependencyManager extends DefaultTask {
             List<DownloadedDependency> downloadedDependencies = new ArrayList<DownloadedDependency>();
 
             // Delete output directory
-            FileUtils.deleteDirectory(Paths.get(getProject().getBuildDir().toString(),DEFAULT_LOCATION).toFile());
-            FileUtils.deleteQuietly(Paths.get(getProject().getBuildDir().toString(),DEFAULT_DEPENDENCY_FILE).toFile());
-            FileUtils.deleteQuietly(Paths.get(getProject().getBuildDir().toString(),DEFAULT_DEPENDENCY_BASE_FILE).toFile());
+            FileUtils.deleteDirectory(Paths.get(getProject().getBuildDir().toString(), DEFAULT_LOCATION).toFile());
+            FileUtils.deleteQuietly(Paths.get(getProject().getBuildDir().toString(), DEFAULT_DEPENDENCY_FILE).toFile());
+            FileUtils.deleteQuietly(Paths.get(getProject().getBuildDir().toString(), DEFAULT_DEPENDENCY_BASE_FILE).toFile());
             // Check delta dependency
             List<DownloadedDependency> baseDependencies = getBaseDependencies();
 
             int iteration = 0;
-            if (dpExtension.getConfigurations() != null) {
-                for (String[] configurationArray : dpExtension.getConfigurations()) {
-                    if (dpExtension.getStripVersion() != null)
-                        stripVersion = ((stripVersion = dpExtension.getStripVersion()[iteration]) != null) ? stripVersion : true;
-                    if (dpExtension.getDecompress() != null)
-                        decompress = ((decompress = dpExtension.getDecompress()[iteration]) != null) ? decompress : true;
+            Preconditions.checkNotNull(dpExtension.getConfigurations(), "No configuration found for plugin dependency-manager");
+            for (String[] configurationArray : dpExtension.getConfigurations()) {
+                if (dpExtension.getStripVersion() != null)
+                    stripVersion = ((stripVersion = dpExtension.getStripVersion()[iteration]) != null) ? stripVersion : true;
+                if (dpExtension.getDecompress() != null)
+                    decompress = ((decompress = dpExtension.getDecompress()[iteration]) != null) ? decompress : true;
 
-                    for (String configuration : configurationArray) {
-                        Configuration gradleConfiguration = getProject().getConfigurations().getByName(configuration);
-                        ResolvedConfiguration resolvedConfiguration = gradleConfiguration.getResolvedConfiguration();
+                for (String configuration : configurationArray) {
+                    Configuration gradleConfiguration = getProject().getConfigurations().getByName(configuration);
+                    ResolvedConfiguration resolvedConfiguration = gradleConfiguration.getResolvedConfiguration();
 
-                        for (ResolvedArtifact artifact : resolvedConfiguration.getResolvedArtifacts()) {
-                            DownloadedDependency downloadedDependency = copyDependency(artifact,
-                                    Paths.get(getProject().getBuildDir().toString(), DEFAULT_LOCATION, configuration),
-                                    stripVersion,
-                                    decompress,
-                                    configuration,
-                                    baseDependencies);
-                            if (downloadedDependency != null) {
-                                downloadedDependencies.add(downloadedDependency);
-                            }
-                        }
+                    for (ResolvedArtifact artifact : resolvedConfiguration.getResolvedArtifacts()) {
+                        DownloadedDependency downloadedDependency = copyDependency(artifact,
+                                Paths.get(getProject().getBuildDir().toString(), DEFAULT_LOCATION, configuration),
+                                stripVersion,
+                                decompress,
+                                configuration,
+                                baseDependencies);
+                        downloadedDependencies.add(downloadedDependency);
                     }
-
-                    iteration++;
                 }
-            }else{
-                getProject().getLogger().warn("No configuration found for plugin dependency-manager");
+
+                iteration++;
             }
-
             generateResolvedDependenciesFile(downloadedDependencies);
-
-
-        }catch(Exception e){
+        } catch (Exception e) {
             getProject().getLogger().error("Error downloading dependencies " + e.toString());
             throw e;
         }
@@ -88,15 +79,16 @@ public class DependencyManager extends DefaultTask {
     private List<DownloadedDependency> getBaseDependencies() throws IOException {
         List<DownloadedDependency> baseDependencies = null;
         Configuration baseConfiguration = getProject().getConfigurations().getByName(BASE_CONFIGURATION);
-        if (baseConfiguration != null){
+        if (baseConfiguration != null) {
             ResolvedConfiguration resolvedBaseConfiguration = baseConfiguration.getResolvedConfiguration();
             for (ResolvedArtifact artifact : resolvedBaseConfiguration.getResolvedArtifacts()) {
-                if (artifact.getExtension().equals("dependencies")){
+                if (artifact.getExtension().equals("dependencies")) {
                     Path baseLocation = Paths.get(getProject().getBuildDir().toString(), DEFAULT_DEPENDENCY_BASE_FILE);
                     Files.createDirectories(baseLocation.getParent());
-                    Files.copy(artifact.getFile().toPath() , baseLocation , StandardCopyOption.REPLACE_EXISTING);
+                    Files.copy(artifact.getFile().toPath(), baseLocation, StandardCopyOption.REPLACE_EXISTING);
                     ObjectMapper mapper = new ObjectMapper();
-                    baseDependencies = mapper.readValue(Files.readString(baseLocation), new TypeReference<List<DownloadedDependency>>(){});
+                    baseDependencies = mapper.readValue(Files.readString(baseLocation), new TypeReference<List<DownloadedDependency>>() {
+                    });
                     break;
                 }
             }
@@ -110,11 +102,13 @@ public class DependencyManager extends DefaultTask {
         DownloadedDependency downloadedDependency = new DownloadedDependency(artifact, stripVersion, decompress, isExtensionToDecompress(artifact.getExtension()), configuration, downloadPath);
         // Search for the reason in dependency list as it is not informed in resolved artifact
         final Dependency reason = getDependencyFromArtifact(artifact, getProject().getConfigurations().getByName(configuration).getIncoming().getDependencies());
-        requireNonNull(reason, "Required dependency is null");
-        downloadedDependency.setReason(reason.getReason());
+
+        if (reason != null) {
+            downloadedDependency.setReason(reason.getReason());
+        }
 
         // If already in base dependency it will not be added / processed
-        if (!downloadedDependency.findIn(baseDependencies)){
+        if (!downloadedDependency.findIn(baseDependencies)) {
             downloadedDependency.setDifferentFromBase(true);
             copyArtifact(artifact, decompress, Paths.get(downloadedDependency.getLocation()));
         }
@@ -122,25 +116,25 @@ public class DependencyManager extends DefaultTask {
         return downloadedDependency;
     }
 
-    private void copyArtifact(ResolvedArtifact artifact, boolean decompress, Path dest) throws Exception{
-        if (decompress && isExtensionToDecompress(artifact.getExtension())){
-            if (artifact.getExtension().equalsIgnoreCase("zip")){
+    private void copyArtifact(ResolvedArtifact artifact, boolean decompress, Path dest) throws Exception {
+        if (decompress && isExtensionToDecompress(artifact.getExtension())) {
+            if (artifact.getExtension().equalsIgnoreCase("zip")) {
                 CompressionUtils.unZipFile(artifact.getFile(), dest.toFile());
-            }else{
-                if (artifact.getExtension().equalsIgnoreCase("tar.gz") || artifact.getExtension().equalsIgnoreCase("tgz")){
+            } else {
+                if (artifact.getExtension().equalsIgnoreCase("tar.gz") || artifact.getExtension().equalsIgnoreCase("tgz")) {
                     CompressionUtils.unTarFile(artifact.getFile(), dest.toFile());
                 }
             }
-        }else {
+        } else {
             Files.createDirectories(dest.getParent());
-            Files.copy(artifact.getFile().toPath() , dest, StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(artifact.getFile().toPath(), dest, StandardCopyOption.REPLACE_EXISTING);
         }
     }
 
     private Dependency getDependencyFromArtifact(ResolvedArtifact artifact, DependencySet dependencies) {
         Dependency foundDependency = null;
         for (Dependency dependency : dependencies) {
-            if (dependency.getName().equals(artifact.getName())){
+            if (dependency.getName().equals(artifact.getName())) {
                 foundDependency = dependency;
                 break;
             }
@@ -158,15 +152,11 @@ public class DependencyManager extends DefaultTask {
         mapper.writerWithDefaultPrettyPrinter().writeValue(downloadedDependenciesFile, downloadedDependencies);
     }
 
-    private boolean isExtensionToDecompress(String extension){
+    private boolean isExtensionToDecompress(String extension) {
         return (extension.equalsIgnoreCase("zip")
                 || extension.equalsIgnoreCase("tgz")
                 || extension.equalsIgnoreCase("tar.gz"));
     }
-
-
-
-
 
 
 }
