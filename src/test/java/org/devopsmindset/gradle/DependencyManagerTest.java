c09c15806d8ae2cdaa488825040dc3c0091e4be0
Anyul Rivas <anyul.rivas_oropeza@roche.com>
2021-01-08 09:14:42 +0100

    - adding argument checking. Reducing cyclomatic complexity

--- src/test/java/org/devopsmindset/gradle/DependencyManagerTest.java
+++ src/test/java/org/devopsmindset/gradle/DependencyManagerTest.java
@@ -1,23 +1,26 @@
 package org.devopsmindset.gradle;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 
-public class DependencyManagerTest {
+class DependencyManagerTest {
 
-    private static String testProjectPath = "test-project";
+    private static final String testProjectPath = "test-project";
 
-    @org.junit.jupiter.api.Test
-    void run() throws IOException {
+    @Test
+    void run() {
         File projectDir = new File(testProjectPath);
         BuildResult result = GradleRunner.create()
                 .withProjectDir(projectDir)
                 .withArguments("dependenciesDownload", "--refresh-dependencies", "--stacktrace", "--debug")
                 .withDebug(true)
                 .build();
+
+        Assertions.assertNotNull(result);
     }
 }
